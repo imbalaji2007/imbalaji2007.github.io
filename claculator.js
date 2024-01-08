1c64a2c9f0a1626181f6e368b94879091244f7fc
imbalaji2007 <mwari9897@gmail.com>
2024-01-08 11:31:19 +0530

    Edited Calculator

--- claculator.js
+++ claculator.js
@@ -1,5 +1,6 @@
 var display = document.getElementById('display')
 var cal='';
+var result = '';
 
 function addToDisplay(input) {
     if (input=='*'){
@@ -10,15 +11,32 @@ function addToDisplay(input) {
         display.value += '%';
         cal += '/100';
     }
+    else if (input=='3.1415926535897') {
+        display.value+='π';
+        cal+='3.1415926535897';
+
+    }
+    else if (input=='/'){
+        display.value+='÷';
+        cal+='/'
+    }
     else{
     display.value += input;
     cal += input;
 }
+
 }
 
 function calculate() {
     try {
-        display.value = eval(cal);
+        result = eval(cal);
+        if (result==Infinity || result === -Infinity){
+            display.value='ꝏ';
+        }
+        else{
+            display.value=result;
+        }
+
     } catch (error) {
         display.value = 'Error';
     }
@@ -28,3 +46,7 @@ function clearDisplay() {
     display.value = '';
     cal='';
 }
+
+function back(){
+    display.value=display.value.slice(0,-1);
+    cal=cal.slice(0,-1);}
