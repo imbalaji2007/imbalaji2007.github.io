88186d46f17d49523ebbd05b1af57fcc8385424b
imbalaji2007 <mwari9897@gmail.com>
2024-09-03 18:12:15 +0530

    Changed the format

--- clock.js
+++ clock.js
@@ -1,66 +1,51 @@
 var h1 = document.getElementById('h1');
 
-var date = new Date();
-var hrs=0;
-var mins=0;
-var sec=-1;
+var min = 0;
+var sec = 0;
+var msec = 0;
 var timer;
-var n = -1;
+var isRunning = false;
 
-function start(){
-
-    n=n+1
-
-    if (n===0){
-
-    console.log('Started');
-
-    update();
-    timer = setInterval(update, 1010);
-
-    function update(){
-    sec=Number(sec)+1;
-    mins=Number(mins);
-    hrs=Number(hrs);
-
-    if(sec<10){
-        sec=String('0'+sec);
+function start() {
+    if (!isRunning) {
+        isRunning = true;
+        console.log('Started');
+        timer = setInterval(update, 100); // Updates every 10ms for more precise timing
     }
-    if (sec===60){
-        sec=String('00');
-        mins=Number(mins)+1;
-    }
-    if (mins<10){
-        mins=String('0'+mins);
-    }
-    if (mins===60){
-        mins=String('00');
-        hrs=Number(hrs)+1;
-    }
-    if (hrs<10){
-        hrs=String('0'+hrs)
-    }
-    h1.textContent=hrs+' : '+mins+' : '+sec;}
-            }
-
-    else{
-        console.log("Can't start");
-    }
-
 }
 
-function stop(){
-    n=-1
-    console.log('stop');
-    clearInterval(timer);
+function update() {
+    msec += 1;
+
+    if (msec >= 10) {
+        msec = 0;
+        sec += 1;
+    }
+
+    if (sec >= 60) {
+        sec = 0;
+        min += 1;
+    }
+
+    h1.textContent = 
+        (min < 10 ? '0' + min : min) + ' : ' + 
+        (sec < 10 ? '0' + sec : sec) + ' : ' + 
+        (msec < 100 ? '0' + msec : msec);
 }
 
-function reset(){
-    n=-1
-    clearInterval(timer);
-    console.log('Restarted');
-    h1.textContent='HH : MM : SS';
-    sec=-1;
-    hrs=0;
-    mins=0;
+function stop() {
+    if (isRunning) {
+        isRunning = false;
+        console.log('Stopped');
+        clearInterval(timer);
+    }
+}
+
+function reset() {
+    stop();
+    console.log('Reset');
+    min = 0;
+    sec = 0;
+    msec = 0;
+    h1.textContent = 'MM : SS : MS';
 }
